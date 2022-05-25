15086292e0f0e3eb2988b619e7d1b7dcc5e2abbe
Thaís Horikawa Gomes <thaishorikawa@hotmail.com>
2022-05-24 22:46:29 -0300

    feat: add new java files - first java program

---
+++ bytebank-herdado/src/EditorVideo.java
@@ -0,0 +1,9 @@
+
+public class EditorVideo extends Funcionario {
+
+	public double getBonificacao() {
+		System.out.println("Chama Editor Vídeo: ");
+		return 100;
+	}
+	
+}
---
+++ bytebank-herdado/src/Gerente.java
@@ -0,0 +1,25 @@
+
+public class Gerente extends Funcionario implements Autenticavel {
+
+	private AutenticacaoUtil autenticador;
+
+	public Gerente() {
+		this.autenticador = new AutenticacaoUtil();
+	}
+	
+	public double getBonificacao() {
+		System.out.println("Chamando Gerente: ");
+		return super.getSalario();
+	}
+
+	@Override
+	public void setSenha(int senha) {
+		this.autenticador.setSenha(senha);	
+	}
+
+	@Override
+	public boolean autentica(int senha) {
+		boolean autenticou = this.autenticador.autentica(senha);
+		return autenticou;
+		}	
+}
