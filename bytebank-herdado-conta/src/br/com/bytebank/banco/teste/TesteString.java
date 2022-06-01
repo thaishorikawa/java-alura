2463a1ac43df5c15c55ec0f9faaa282b066d4f86
Thaís Horikawa Gomes <thaishorikawa@hotmail.com>
2022-06-01 16:16:16 -0300

    feat: practicing immutable string

---
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteString.java
@@ -0,0 +1,16 @@
+package br.com.bytebank.banco.teste;
+
+public class TesteString {
+
+	public static void main(String[] args) {
+	
+		String nome = "Alura";
+		//String outro = new String("Alura"); 
+		
+		String outra = nome.replace("A", "a");
+		
+		System.out.println(nome);
+		System.out.println(outra);
+	}
+
+}
