c4bf623cedf701a57ffb3fb8c0c4cb6d9a4e0712
Thaís Horikawa Gomes <thaishorikawa@hotmail.com>
2022-06-01 20:16:10 -0300

    feat: practicing array

---
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteArrayDePrimitivos.java
@@ -0,0 +1,19 @@
+package br.com.bytebank.banco.teste;
+
+public class TesteArrayDePrimitivos {
+
+	
+	//Array
+	public static void main(String[] args) {
+		
+		int[] idades = new int[5]; //inicializa o array com 0
+		
+		for(int i = 0; i < idades.length; i++) {
+			idades[i] = i * i;
+			System.out.println(idades[i]);
+		}
+		
+
+	}
+
+}
--- bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteString.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteString.java
@@ -4,10 +4,17 @@ public class TesteString {
 
 	public static void main(String[] args) {
 	
-		String nome = "Alura";
+		String nome = "Alura"; //object literal
 		//String outro = new String("Alura"); 
 		
 		String outra = nome.replace("A", "a");
+		
+		
+		//char c = 'A';
+		//char d = 'a';
+		//String outra = nome.replace(c, d);
+		
+		//String outra = nome.toUpperCase();
 		
 		System.out.println(nome);
 		System.out.println(outra);
