f5907f3b5ee04a1f51a57967a7ddeaeea9c60e8a
Thaís Horikawa Gomes <thaishorikawa@hotmail.com>
2022-06-04 10:58:02 -0300

    feat: final project java alura

--- bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/ContaCorrente.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/ContaCorrente.java
@@ -1,19 +1,31 @@
 package br.com.bytebank.banco.modelo;
 
-public class ContaCorrente extends Conta {
+//new ContaCorrente()
+public class ContaCorrente extends Conta implements Tributavel {
 
-	public ContaCorrente(int agencia, int numero) {	
+	public ContaCorrente(int agencia, int numero) {
 		super(agencia, numero);
 	}
 	
 	@Override
-	public void saca(double valor) throws SaldoInsuficienteException {
+	public void saca(double valor) throws SaldoInsuficienteException{
 		double valorASacar = valor + 0.2;
 		super.saca(valorASacar);
 	}
 
 	@Override
 	public void deposita(double valor) {
-		super.saldo += valor;
+        super.saldo += valor;
+    }
+
+	@Override
+	public double getValorImposto() {	
+		return super.saldo * 0.01;
 	}
+	
+	@Override
+	public String toString() {
+		return "ContaCorrente, " + super.toString();
+	}
+	
 }
