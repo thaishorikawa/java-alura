b03e245640b8aab5538ba16dec057294cd62b38a
Thaís Horikawa Gomes <thaishorikawa@hotmail.com>
2022-06-05 14:52:17 -0300

    feat: practicing PrintWriter and PrintStream

---
+++ java-io/src/br/com/alura/java/io/teste/TesteEscrita3.java
@@ -0,0 +1,35 @@
+package br.com.alura.java.io.teste;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.Writer;
+
+public class TesteEscrita3 {
+
+	public static void main(String[] args) throws IOException {
+		
+		//Fluxo de Saída com Arquivo
+		
+//		OutputStream fos = new FileOutputStream("lorem2.txt");
+//		Writer osw = new OutputStreamWriter(fos);
+//		BufferedWriter bw = new BufferedWriter(osw);
+		
+//		BufferedWriter bw = new BufferedWriter(new FileWriter("lorem2.txt"));
+//		PrintStream ps = new PrintStream("lorem2.txt");
+		
+		PrintWriter ps = new PrintWriter("lorem2.txt");
+		
+		ps.println("Lorem ipsum dolor sit amet, consectetur");
+		ps.println();
+		ps.println("Olá, meu nome é Thaís");
+		
+		ps.close();
+	}
+
+}
