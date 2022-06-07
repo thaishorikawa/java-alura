2ef717cc052164c9db788b111cea0cc0ea72b486
Thaís Horikawa Gomes <thaishorikawa@hotmail.com>
2022-06-07 18:12:16 -0300

    feat: set operations in java

--- gerenciador-de-cursos/src/br/com/alura/TestaCurso2.java
+++ gerenciador-de-cursos/src/br/com/alura/TestaCurso2.java
@@ -9,7 +9,6 @@ public class TestaCurso2 {
 	public static void main(String[] args) {
 		Curso javaColecoes = new Curso("Dominando as coleções do Java", "Paulo Silveira");
 		
-		//javaColecoes.getAulas().add(new Aula("Trabalhando com ArrayList", 21));
 		javaColecoes.adiciona(new Aula("Trabalhando com ArrayList", 21));
 		javaColecoes.adiciona(new Aula("Modelando com coleções", 22));
 		
