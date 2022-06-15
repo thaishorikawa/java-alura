a76e5df08875ab227ce5aa460c76f8dc55a5f84a
Thaís Horikawa Gomes <thaishorikawa@hotmail.com>
2022-06-14 21:26:22 -0300

    feat: lambda

--- java8/src/OrdenaStrings.java
+++ java8/src/OrdenaStrings.java
@@ -7,45 +7,42 @@ import java.util.function.Consumer;
 public class OrdenaStrings {
 
 	public static void main(String[] args) {
-		
+
 		List<String> palavras = new ArrayList<String>();
 		palavras.add("alura online");
 		palavras.add("editora casa do código");
 		palavras.add("caelum");
+
+		// Collections.sort(palavras, comparador);
 		
-		Comparator<String> comparador = new ComparadorPorTamanho();
+		palavras.sort((s1, s2) -> {
+			if (s1.length() < s2.length())
+				return -1;
+			if (s1.length() > s2.length())
+				return 1;
+			return 0;
+		});
 		
-		
-		//Collections.sort(palavras, comparador);
-		palavras.sort(comparador);
+		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
 		
 		System.out.println(palavras);
-		
+
 //		for (String p : palavras) {
 //			System.out.println(p);
 //		}
 		
-		Consumer<String> consumidor = new ImprimeNaLinha();
-		palavras.forEach(consumidor);
-	}
-}
-
-class ImprimeNaLinha implements Consumer<String> {
-
-	@Override
-	public void accept(String s) {
-		System.out.println(s);
-	}
-}
+		
+		palavras.forEach(s -> System.out.println(s));
+	}}
 
 class ComparadorPorTamanho implements Comparator<String> {
 
 	@Override
 	public int compare(String s1, String s2) {
-		if(s1.length() < s2.length()) 
-            return -1;
-        if(s1.length() > s2.length()) 
-            return 1;
+		if (s1.length() < s2.length())
+			return -1;
+		if (s1.length() > s2.length())
+			return 1;
 		return 0;
 	}
 }
