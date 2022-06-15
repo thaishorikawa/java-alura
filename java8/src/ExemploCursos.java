4270158ce077f70e2a49163da308236ea87e8a8f
Thaís Horikawa Gomes <thaishorikawa@hotmail.com>
2022-06-15 16:37:41 -0300

    feat: streams, collectors and APIs

--- java8/src/ExemploCursos.java
+++ java8/src/ExemploCursos.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.OptionalDouble;
+import java.util.stream.Collectors;
 
 	class Curso {
 		
@@ -32,10 +34,27 @@ public class ExemploCursos {
 			cursos.sort(Comparator.comparing(Curso::getAlunos));
 			//cursos.forEach(c -> System.out.println(c.getNome()));
 			
-			int sum = cursos.stream().filter(c -> c.getAlunos() >= 100).mapToInt(Curso::getAlunos).sum();
+			OptionalDouble media = cursos.stream()
+					.filter(c -> c.getAlunos() > 100)
+					.mapToInt(Curso::getAlunos)
+					.average();
 			
-			System.out.println(sum);
+			//System.out.println(sum);
 			
+//			cursos.stream()
+//				.filter(c -> c.getAlunos() >= 100)
+//				.findAny()
+//				.ifPresent(c -> System.out.println(c.getNome()));
+			
+			cursos.stream()
+				.filter(c -> c.getAlunos() >= 100)
+				.collect(Collectors.toMap(
+						c -> c.getNome(),
+						c -> c.getAlunos()))
+				.forEach((nome, alunos) -> System.out.println(nome + " tem " + alunos + " alunos"));
+			
+//			cursos.stream().
+//			forEach(c -> System.out.println(c.getNome()));
 			
 			
 		}
